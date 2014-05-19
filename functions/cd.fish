03728451f52d1ce3fd88dbc240e0ba97d08ef3db
jaseg <code@jaseg.net>
2014-05-19 10:02:37 +0200

    Pimped cd a bit more

--- functions/cd.fish
+++ functions/cd.fish
@@ -1,5 +1,10 @@
 # Modified cd that also displays the directory's contents if the listing is less than 5 lines long
 function cd
+	if test -n "$argv"
+		if test -e $argv -a ! -d (realpath $argv)
+			set argv (dirname $argv)
+		end
+	end
 	builtin cd $argv
-	test (ls -C -w $COLUMNS |wc -l) -le 5; and ls
+	and test (ls -C -w $COLUMNS |wc -l) -le 5; and ls
 end
